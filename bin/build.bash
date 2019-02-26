aceec5f960ffb2b768fd42d20e40aff153d4665d
Judson Neer <judson.neer@gmail.com>
2019-02-25 16:46:23 -0800

    Add credits line that auto-populates a version number.

--- bin/build.bash
+++ bin/build.bash
@@ -7,4 +7,6 @@ cd "$(dirname "$0")/.."
 
 mkdir -p dist
 
-docker run --rm -v $(pwd):/data lordjabez/xelatex xelatex -halt-on-error -output-directory dist resume.tex
+VERSION=${TRAVIS_TAG-$(git describe)}
+
+docker run --rm -v $(pwd):/data lordjabez/xelatex xelatex -halt-on-error -output-directory dist "\def\version{$VERSION}\input resume.tex"
